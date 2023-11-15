4e91662bdbba8a7b727f60e8aa731fe40995cb3c
Ubuntu <ubuntu@ip-172-31-95-202.ec2.internal>
2023-11-15 13:26:23 +0000

    backend

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -30,13 +30,6 @@ rm -rf /var/www/html/*
 # Descargamos el código fuente de WordPress en /var/www/html
 wp core download --locale=es_ES --path=/var/www/html --allow-root
 
-# Creamos la base de datos y el usuario de la base de datos 
-mysql -u root <<< "DROP DATABASE IF EXISTS $WORDPRESS_DB_NAME"
-mysql -u root <<< "CREATE DATABASE $WORDPRESS_DB_NAME"
-mysql -u root <<< "DROP USER IF EXISTS $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
-mysql -u root <<< "CREATE USER $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL IDENTIFIED BY '$WORDPRESS_DB_PASSWORD'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO $WORDPRESS_DB_USER@$IP_CLIENTE_MYSQL"
-
 # Creamos el archivo wp-config 
 wp config create \
   --dbname=$WORDPRESS_DB_NAME \
